cf2edb308f54814328a4014e2b02a6d57db1d36b
Max Rubel <maxrubel1@gmail.com>
2024-05-24 16:29:40 -0500

    looking good

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ var gameData GameData
 var mousePos MousePositions
 var posF string
 var choosePlayerMessage ChoosePlayerMessage
+var trackingMouse bool
 
 func choosePlayer(no uint8) playerHasBeenChosen {
 	var m playerHasBeenChosen
@@ -76,9 +77,17 @@ func choosePlayer(no uint8) playerHasBeenChosen {
 	return m
 }
 
-func mouseClicked(m MouseMessage) {
+func handleMouseClicked(incoming []byte, conn *websocket.Conn) {
+	var m MouseMessage
 	fmt.Println("this player scored:", m.Player)
+
+	err := binary.Read(bytes.NewReader(incoming[1:]), binary.LittleEndian, &m)
+	if err != nil {
+		fmt.Println("Error decoding mouse message:", err)
+	}
+
 	switch m.Player {
+
 	case 1:
 		gameData.Player1Score++
 	case 2:
@@ -86,8 +95,51 @@ func mouseClicked(m MouseMessage) {
 	default:
 		fmt.Println("Unable to process mouse click")
 	}
+
+	il := strconv.Itoa(gameData.Player1Score)
+	i2 := strconv.Itoa(gameData.Player2Score)
+	is := "n" + "/" + il + "/" + i2
+
+	err = conn.WriteMessage(websocket.TextMessage, []byte(is))
+	if err != nil {
+		log.Println("Write:", err)
+		return
+	}
 	fmt.Println("player1:", gameData.Player1Score)
 	fmt.Println("player2:", gameData.Player2Score)
+}
+
+func handleMousePos(incoming []byte) {
+	var message Message
+	err := binary.Read(bytes.NewReader(incoming[1:]), binary.LittleEndian, &message)
+	if err != nil {
+		fmt.Println("Error decoding mouse message:", err)
+	}
+
+	if message.Player == 1 {
+		mousePos.P1x = int(message.X)
+		mousePos.P1y = int(message.Y)
+	}
+	if message.Player == 2 {
+		mousePos.P2x = int(message.X)
+		mousePos.P2y = int(message.Y)
+	}
+}
+
+func handleChoosePlayer(incoming []byte, conn *websocket.Conn) {
+	err := binary.Read(bytes.NewReader(incoming[1:]), binary.LittleEndian, &choosePlayerMessage)
+	if err != nil {
+		fmt.Println("Error decoding mouse message:", err)
+	}
+	p := choosePlayer(choosePlayerMessage.PlayerChoice)
+	pF := fmt.Sprintf("%d%d", p.MessageType, p.PlayerNo)
+	if p.MessageType == 2 {
+		err = conn.WriteMessage(websocket.TextMessage, []byte(pF))
+		if err != nil {
+			fmt.Println("Error writing choose player msg")
+			return
+		}
+	}
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -110,59 +162,19 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 
 		switch messageType {
 		case 0:
-			var message Message
-			err = binary.Read(bytes.NewReader(incoming[1:]), binary.LittleEndian, &message)
-			if err != nil {
-				fmt.Println("Error decoding mouse message:", err)
-				continue
-			}
-
-			if message.Player == 1 {
-				mousePos.P1x = int(message.X)
-				mousePos.P2y = int(message.Y)
-			}
-			if message.Player == 2 {
-				mousePos.P2x = int(message.X)
-				mousePos.P2y = int(message.Y)
-			}
-
+			go handleMousePos(incoming)
 		case 1:
-			var mouseMessage MouseMessage
-			err = binary.Read(bytes.NewReader(incoming[1:]), binary.LittleEndian, &mouseMessage)
-			if err != nil {
-				fmt.Println("Error decoding mouse message:", err)
-				continue
-			}
-
-			mouseClicked(mouseMessage)
-
-			il := strconv.Itoa(gameData.Player1Score)
-			i2 := strconv.Itoa(gameData.Player2Score)
-			is := "n" + "/" + il + "/" + i2
-
-			err = conn.WriteMessage(websocket.TextMessage, []byte(is))
-			if err != nil {
-				log.Println("Write:", err)
-				return
-			}
-
+			go handleMouseClicked(incoming, conn)
 		case 2:
-			err = binary.Read(bytes.NewReader(incoming[1:]), binary.LittleEndian, &choosePlayerMessage)
-			if err != nil {
-				fmt.Println("Error decoding mouse message:", err)
-				continue
-			}
-			p := choosePlayer(choosePlayerMessage.PlayerChoice)
-			pF := fmt.Sprintf("%d%d", p.MessageType, p.PlayerNo)
-			if p.MessageType == 2 {
-				err = conn.WriteMessage(websocket.TextMessage, []byte(pF))
-			}
+			go handleChoosePlayer(incoming, conn)
 
 		default:
 			fmt.Println("Unknown message type:", messageType)
 		}
+
 		ticker := time.NewTicker(50 * time.Millisecond)
 		defer ticker.Stop()
+
 		select {
 		case <-ticker.C:
 			posF = fmt.Sprintf("m/%d/%d/%d/%d", mousePos.P1x, mousePos.P1y, mousePos.P2x, mousePos.P2y)
@@ -175,7 +187,6 @@ func main() {
 	http.HandleFunc("/ws", wsHandler)
 
 	log.Println("Server starting on port 8080...")
-
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe error:", err)
